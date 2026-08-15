3c490f5f6cf4ad31ed04a49599ca689519fac0bc
Mason S. <10767492@uvu.edu>
2026-08-15 14:02:26 -0600

    Loading RPG tutorial project onto a repo

---
+++ 3d-rpg/Enemy/enemy.gd
@@ -0,0 +1,36 @@
+extends CharacterBody3D
+class_name Enemy
+
+@export var max_health: float = 20.0
+@export var heavy_attack_damage : float = 20.0
+
+@onready var rig: Node3D = $Rig
+@onready var health_component: HealthComponent = $HealthComponent
+@onready var collision_shape_3d: CollisionShape3D = $CollisionShape3D
+@onready var player_detector: ShapeCast3D = $Rig/PlayerDetector
+@onready var area_attack: ShapeCast3D = $Rig/AreaAttack
+
+@onready var player : Player = get_tree().get_first_node_in_group("Player")
+
+func _ready() -> void:
+	rig.set_active_mesh(rig.villager_meshes.pick_random())
+	health_component.set_max_health(max_health)
+
+func _physics_process(delta: float) -> void:
+	if rig.is_idle():
+		check_for_attacks()
+
+func check_for_attacks() -> void:
+	for collision_id in player_detector.get_collision_count():
+		var collider = player_detector.get_collider(collision_id)
+		if collider is Player:
+			rig.travel("Overhead")
+
+func _on_health_component_defeat() -> void:
+	player.stats.level_up()
+	rig.travel("Defeat")
+	collision_shape_3d.disabled = true
+
+
+func _on_rig_heavy_attack() -> void:
+	area_attack.deal_damage(heavy_attack_damage)
---
+++ 3d-rpg/Resources/CharacterStats.gd
@@ -0,0 +1,57 @@
+extends Resource
+class_name CharacterStats
+
+class Ability:
+	var min_modifier : float
+	var max_modifier : float
+	
+	var ability_score : int = 25:
+		set(value):
+			ability_score = clampi(value, 0, 100)
+	
+	func _init(min: float, max: float) -> void:
+		min_modifier = min
+		max_modifier = max
+	
+	func percentile_lerp(min_bound : float, max_bound : float) -> float:
+		return lerp(min_bound, max_bound, ability_score/100.0)
+		
+	func get_modifier() -> float:
+		return percentile_lerp(min_modifier, max_modifier)
+	
+	func increase() -> void:
+		ability_score += randi_range(2, 5)
+
+var level := 1
+var xp := 0
+
+# Damage Bonus on attack.
+var strength := Ability.new(2.0, 12.0)
+# Movement speed in m/s
+var speed := Ability.new(3.0, 7.0)
+# Added health per level
+var endurance := Ability.new(5.0, 25.0)
+# Critical hit chance
+var agility := Ability.new(0.05, 0.25)
+
+func get_base_speed() -> float:
+	return speed.get_modifier()
+
+func get_damage_modifier() -> float:
+	return strength.get_modifier()
+
+func get_crit_chance() -> float:
+	return agility.get_modifier()
+
+func level_up() -> void:
+	level += 1
+	strength.increase()
+	agility.increase()
+	speed.increase()
+	endurance.increase()
+	
+	printt(strength.ability_score, 
+	agility.ability_score, 
+	speed.ability_score,
+	endurance.ability_score
+	)
